5ff202d482226d9637bb6431c83288feca504662
sacleuxa <147699402+sacleuxa@users.noreply.github.com>
2023-11-28 16:55:36 +0100

    css sheet problems

--- index.php
+++ index.php
@@ -2,17 +2,21 @@
   //find header.php
   get_header(); ?>
 
-    <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
-    <script src="js/script.js" defer></script>
-</head>
+    <h1><?php bloginfo( 'name' ); ?></h1>
+    <h2><?php bloginfo( 'description' ); ?></h2>
+ 
+    <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+ 
+    <h3><?php the_title(); ?></h3>
+ 
+    <?php the_content(); ?>
+    
+    <?php wp_link_pages(); ?>
 
-
+    
+    <?php endwhile; ?>
+    <?php endif; ?>
+ 
+    <?php 
+    //get footer from footer.php
+    get_footer(); ?>
